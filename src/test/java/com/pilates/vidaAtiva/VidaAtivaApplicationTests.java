62923e396ffdc4d36d8c2e0be7aff0faf1a4a66e
Emenesys <cjc.cleber@gmail.com>
2019-12-12 17:28:26 -0300

    Project Created

---
+++ src/test/java/com/pilates/vidaAtiva/VidaAtivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilates.vidaAtiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaAtivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
